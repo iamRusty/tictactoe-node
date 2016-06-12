13a55ff9b051c59cf53546169f8e030be3098c3f
iamRusty <rustymina@gmail.com>
2016-06-12 10:52:34 +0800

    Added image support

--- server.js
+++ server.js
@@ -1,14 +1,51 @@
-var http = require("http");
+var http = require('http');
+var fs = require('fs');
+var url = require('url');
 
-http.createServer(function (request, response) {
 
-   // Send the HTTP header 
-   // HTTP Status: 200 : OK
-   // Content Type: text/plain
-   response.writeHead(200, {'Content-Type': 'text/plain'});
+// Create a server
+http.createServer( function (request, response) {  
+   // Parse the request containing file name
+   var pathname = url.parse(request.url).pathname;
    
-   // Send the response body as "Hello World"
-   response.end('Hello World\n');
+   // Print the name of the file for which request is made.
+   console.log("Request for " + pathname + " received.");
+
+   // File Type Extraction
+   var file_type = pathname.substr(1).split(".")[1];
+   
+   
+   // Read the requested file content from file system
+   fs.readFile(pathname.substr(1), function (err, data) {
+      if (err) {
+         console.log(err);
+         // HTTP Status: 404 : NOT FOUND
+         // Content Type: text/plain
+
+         response.writeHead(404, {'Content-Type': 'text/html'});
+      }else{	
+         //Page found	  
+         // HTTP Status: 200 : OK
+         // Content Type: text/plain
+         console.log(file_type);
+
+         if (file_type == "html") {
+            response.writeHead(200, {'Content-Type': 'text/html'});	
+            // Write the content of the file to response body
+            response.write(data.toString());	
+         }
+         else if (file_type == "css") {
+             response.writeHead(200, {'Content-Type': 'text/css'});
+         }
+         else {
+            var content_type = "image/" + file_type;
+            response.writeHead(200, {'Content-Type': content_type});
+            response.write(data)
+         }         	
+      }
+      // Send the response body 
+      response.end();
+   });   
 }).listen(8081);
 
 // Console will print the message
